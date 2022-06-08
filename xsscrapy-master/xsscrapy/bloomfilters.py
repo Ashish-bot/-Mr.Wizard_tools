e023b591a07650062a1366ecd75c36566a8068c5
Mr.Wizard <77635994+Ashish-bot@users.noreply.github.com>
2022-06-08 10:53:12 -0400

    Add files via upload

---
+++ xsscrapy-master/xsscrapy/bloomfilters.py
@@ -0,0 +1,24 @@
+from pybloomfilter import BloomFilter
+from scrapy.utils.job import job_dir
+from scrapy.dupefilters import BaseDupeFilter
+from xsscrapy.settings import bloomfilterSize
+
+class BloomURLDupeFilter(BaseDupeFilter):
+    """Request Fingerprint duplicates filter"""
+
+    def __init__(self, path=None):
+        self.file = None
+        self.fingerprints = BloomFilter(bloomfilterSize*10, 0.0001)
+
+    @classmethod
+    def from_settings(cls, settings):
+        return cls(job_dir(settings))
+
+    def request_seen(self, request):
+        fp = request.url
+        if fp in self.fingerprints:
+            return True
+        self.fingerprints.add(fp)
+
+    def close(self, reason):
+        self.fingerprints = None
